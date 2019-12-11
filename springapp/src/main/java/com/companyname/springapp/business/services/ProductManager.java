3f4be528295ef7f82e9391429b0f8e8771958a36
PedrassoxD <pedroperlab@gmail.com>
2019-12-11 21:18:40 +0100

    springappMVC 5

---
+++ springapp/src/main/java/com/companyname/springapp/business/services/ProductManager.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.companyname.springapp.business.services;
+
+import java.util.List;
+
+import com.companyname.springapp.business.entities.Product;
+/**
+ * 
+ * @author usuario
+ *
+ */
+public interface ProductManager {
+
+    public void increasePrice(int percentage);
+    
+    public List<Product> getProducts();
+
+}
